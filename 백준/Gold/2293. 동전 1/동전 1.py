35a1a71557a0ca4c79bf217897605955f1476c5e
jiwonKim <78153919+jeeewonkim@users.noreply.github.com>
2024-04-20 15:38:01 +0900

    [Gold V] Title: 동전 1, Time: 192 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+coin = list(int(input()) for _ in range(n))
+coin.sort()
+dp = [0] *(k+1)
+dp[0] = 1
+for c in coin:
+    for i in range(c, k+1):
+        dp[i] += dp[i-c]
+print(dp[k])
